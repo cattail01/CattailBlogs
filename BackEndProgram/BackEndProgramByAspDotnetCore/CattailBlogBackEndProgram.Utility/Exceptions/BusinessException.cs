e24094ba2e65ebc299f64593d44f4a7172b20d98
cattail <334750034@qq.com>
2023-12-13 22:50:30 +0800

    [process] 设计全局异常处理

---
+++ BackEndProgram/BackEndProgramByAspDotnetCore/CattailBlogBackEndProgram.Utility/Exceptions/BusinessException.cs
@@ -0,0 +1,18 @@
+﻿namespace CattailBlogBackEndProgram.Utility.Exceptions;
+
+/// <summary>
+/// 业务异常类
+/// </summary>
+/// <remarks>
+/// <para>携带一个返回值ResultSingleton类型成员</para>
+/// <para>todo 后期会根据异常类型增加增加继承自该异常类的类</para>
+/// </remarks>
+public class BusinessException : Exception
+{
+    public ResultSingleton ResultSingleton { get; private set; }
+
+    public BusinessException(ResultSingleton resultSingleton)
+    {
+        ResultSingleton = resultSingleton;
+    }
+}
---
+++ BackEndProgram/BackEndProgramByAspDotnetCore/CattailBlogBackEndProgram.Web/GlobalExceptionHandler/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CattailBlogBackEndProgram.Utility.GlobalExceptionHandler;
+
+public class GlobalExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    
+    public GlobalExceptionHandlerMiddleware(RequestDelegate  next)
+    {
+        _next = next;
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        JsonResult result;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+}
