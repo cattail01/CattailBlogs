853a684a1c757ea730bdb551dc2f2b4567564d9f
cattail <334750034@qq.com>
2023-12-18 12:16:46 +0800

    [test] 尝试制作前端，但是失败

---
+++ FrontEndProgram/project/src/router/index.js
@@ -0,0 +1,20 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+import Login from "@/views/Login.vue";
+
+const routes = [
+    {
+        path: "/",
+        redirect: "/Test"
+    },
+    {
+        path: "/Login",
+        name: "Login",
+        component: Login
+    }
+]
+
+const router = createRouter(
+    {history: createWebHashHistory(), routes: routes}
+);
+
+export default router
